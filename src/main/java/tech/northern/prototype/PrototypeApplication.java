cd82e7ccf5cf60691a0f6d753fc38d28cfbbb2f6
HopeBing <1259713996@qq.com>
2019-12-21 21:25:40 +0800

    引入mybatis、devtools

--- src/main/java/tech/northern/prototype/PrototypeApplication.java
+++ src/main/java/tech/northern/prototype/PrototypeApplication.java
@@ -2,9 +2,17 @@ package tech.northern.prototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PrototypeApplication {
+
+	@RequestMapping("/")
+	String index() {
+		return "hello spring boot";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrototypeApplication.class, args);
